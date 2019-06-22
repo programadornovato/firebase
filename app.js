b73d4bc4db3a0617a15f5e46f391aeef280eafb6
Programador Novato <info@programadornovato.com>
2019-06-21 22:18:58 -0500

    mover script

---
+++ app.js
@@ -0,0 +1,94 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBFgJ_abbrHfju-0uE70fIgeDb4zSuZI40",
+    authDomain: "usuarios-bf872.firebaseapp.com",
+    databaseURL: "https://usuarios-bf872.firebaseio.com",
+    projectId: "usuarios-bf872",
+    storageBucket: "",
+    messagingSenderId: "296481844448",
+    appId: "1:296481844448:web:e1a463f1e90a161e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var textoVerificado = "";
+        if (emailVerified === false) {
+            textoVerificado = "Email no verificado";
+        }
+        else {
+            textoVerificado = "Email verificado";
+        }
+        var providerData = user.providerData;
+        document.getElementById('login').innerHTML =
+            `<p>Logueado ` + user.email + ` ` + textoVerificado + ` <p>
+        <button  type="button" class="btn btn-danger" onclick="cerrar()">Cerra sesion</button>
+    `;
+        console.log(user);
+    } else {
+        document.getElementById('login').innerHTML = "No Logueado ";
+    }
+});
+
+function enviar() {
+    var email = document.getElementById('email').value;
+    var pass = document.getElementById('pass').value;
+    firebase.auth().createUserWithEmailAndPassword(email, pass)
+        .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage);
+        })
+        .then(function () {
+            verificar();
+        });
+}
+function verificar() {
+    var user = firebase.auth().currentUser;
+    user.sendEmailVerification().then(function () {
+        // Email sent.
+    }).catch(function (error) {
+        // An error happened.
+    });
+}
+function acceso() {
+    var emailA = document.getElementById('emailA').value;
+    var passA = document.getElementById('passA').value;
+    firebase.auth().signInWithEmailAndPassword(emailA, passA)
+        .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage);
+        });
+}
+function cerrar() {
+    firebase.auth().signOut()
+        .then(function () {
+            console.log('Salir');
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+$(document).ready(function () {
+    $('#loginRestro').change(function () {
+        if ($(this).is(':checked')) {
+            $('#areaLogin').hide();
+            $('#areaRegistro').show();
+        }
+        else {
+            $('#areaLogin').show();
+            $('#areaRegistro').hide();
+        }
+    });
+});
+
